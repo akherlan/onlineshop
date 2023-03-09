082ca8e81d3aba7ffeeaaf387955dd557f2a1b0e
Andi <getdata@duck.com>
2023-03-09 11:46:16 +0700

    better error handling

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-import {
-  shopeeInstance,
-  getPromotionId,
-  getItemId,
-  getFlashSale
-} from "./shopee.js";
+import { Shopee, ShopeeFlashSale } from "./shopee.js";
+import * as fs from 'fs';
 
-const instance = await shopeeInstance();
-const promotionData = await getPromotionId(instance);
-const activePromo = await promotionData.data[0]; // 0 is current
-console.log("Retrieve data %s", activePromo.name);
-const itemList = await getItemId(instance, activePromo.url, 34); // fashion muslim
-const productList = await getFlashSale(instance, activePromo.promotionid, itemList);
-console.log(await productList);
+
+const getShopeeFlashSaleFashionMuslim = async() => {
+  // look into flash sale promotion list
+  const promotionData = await ShopeeFlashSale.getPromotion();
+  const session = 0; // 0 is current flash sale session
+  const activePromo = await promotionData[session];
+  console.info("Retrieve data %s", activePromo.name);
+  const itemList = await ShopeeFlashSale.getItem(activePromo.url, 34);
+  // 34 is fashion muslim category, look at ShopeeFlashSale.getCategory(0) for more
+  const productList = await ShopeeFlashSale.getFlashSale(activePromo.promotionid, itemList);
+  console.info("Item count: %s", productList.length);
+  return productList;
+}
+
+// var fashionMusilim = await getShopeeFlashSaleFashionMuslim();
+// console.log(fashionMusilim);
+
+console.log(await Shopee.getCategory());
